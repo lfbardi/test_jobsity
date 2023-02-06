1f5615e071055451292de9f7a24b85c2fb1fb836
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-02-05 22:24:20 -0300

    list all shows datasource

---
+++ test_jobsity/lib/features/list_all_shows/data/list_all_shows_datasource.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:test_jobsity/core/error/exceptions.dart';
+
+import 'models/show.dart';
+
+abstract class ListAllShowsDatasource {
+  Future<List<Show>> getAllShows({
+    required int page,
+  });
+}
+
+class ListAllShowsDatasourceImpl implements ListAllShowsDatasource {
+  final Dio dio;
+
+  ListAllShowsDatasourceImpl({
+    required this.dio,
+  });
+
+  @override
+  Future<List<Show>> getAllShows({
+    required int page,
+  }) async {
+    final response = await dio.get(
+      '/shows?page=$page',
+    );
+
+    if (response.statusCode == 404) {
+      throw ServerException(message: 'End of List');
+    }
+
+    return (response.data as List).map((show) => Show.fromMap(show)).toList();
+  }
+}
