9c36674b4dabf8da443061788b4d241a02ca1943
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-02-05 22:21:03 -0300

    added colors and text styles for design system

---
+++ test_jobsity/lib/core/design_system/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFF17181B);
+
+const headerColor = Color(0xFFEEF1F4);
+
+const primaryColor = Color(0xFFF8D458);
+
+const descriptionColor = Color(0xffBCBCBC);
+
+const descriptionColor2 = Color(0xffD5D5D5);
---
+++ test_jobsity/lib/core/design_system/text_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'colors.dart';
+
+final title = GoogleFonts.poppins(
+  color: descriptionColor,
+  fontSize: 20,
+);
+
+final tabText = GoogleFonts.poppins(
+  color: primaryColor,
+  fontSize: 14,
+);
+
+final showRating = GoogleFonts.poppins(
+  color: backgroundColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+final whiteTitle = GoogleFonts.poppins(
+  color: headerColor,
+  fontSize: 20,
+);
