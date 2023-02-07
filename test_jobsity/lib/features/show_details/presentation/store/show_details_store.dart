e7e48f45130ed054b8cdb6d718025cce720c7541
Luis Felipe Bardi <luisfelipe@lalist.com.br>
2023-02-07 17:14:12 -0300

    added favorites repository into show details store

--- test_jobsity/lib/features/show_details/presentation/store/show_details_store.dart
+++ test_jobsity/lib/features/show_details/presentation/store/show_details_store.dart
@@ -2,6 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_jobsity/features/show_details/data/models/season.dart';
 
 import '../../../../core/tv_maze_dio_provider.dart';
+import '../../../favorites_shows/data/favorite_shows_repository.dart';
+import '../../../favorites_shows/presentation/store/favorites_show_store.dart';
 import '../../../list_all_shows/data/models/show.dart';
 import '../../data/models/episode.dart';
 import '../../data/show_details_datasource.dart';
@@ -25,14 +27,20 @@ final showDetailsStoreProvider =
         (ref) {
   return ShowDetailsStore(
     showDetailsRepository: ref.read(showDetailsRepository),
+    favoritesShowsRepository: ref.read(favoritesShowsRepository),
   );
 });
 
 class ShowDetailsStore extends StateNotifier<ShowDetailsState> {
   final ShowDetailsRepository showDetailsRepository;
+  final FavoriteShowsRepository favoritesShowsRepository;
 
-  ShowDetailsStore({required this.showDetailsRepository})
-      : super(InitialShowDetailsState());
+  ShowDetailsStore({
+    required this.showDetailsRepository,
+    required this.favoritesShowsRepository,
+  }) : super(InitialShowDetailsState());
+
+  bool isFavorite = false;
 
   getShowDetails({required int id}) async {
     state = LoadingShowDetailsState();
@@ -105,5 +113,33 @@ class ShowDetailsStore extends StateNotifier<ShowDetailsState> {
     }
 
     return seasons;
+  }
+
+  addFavoriteShow(Show show) async {
+    final addFavoriteEither =
+        await favoritesShowsRepository.addFavoriteShow(show);
+
+    addFavoriteEither.fold(
+      (failure) {
+        state = ErrorShowDetailsState(errorMessage: failure.errorMessage);
+      },
+      (success) {
+        isFavorite = true;
+      },
+    );
+  }
+
+  removeFavoriteShow(int id) async {
+    final removeFavoriteShowEither =
+        await favoritesShowsRepository.removeFavoriteShow(id);
+
+    removeFavoriteShowEither.fold(
+      (failure) {
+        state = ErrorShowDetailsState(errorMessage: failure.errorMessage);
+      },
+      (success) {
+        isFavorite = false;
+      },
+    );
   }
 }
--- test_jobsity/lib/features/show_details/presentation/ui/show_details_page.dart
+++ test_jobsity/lib/features/show_details/presentation/ui/show_details_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_jobsity/core/design_system/colors.dart';
 import 'package:test_jobsity/core/design_system/text_styles.dart';
+import 'package:test_jobsity/features/favorites_shows/presentation/store/favorites_show_store.dart';
+import 'package:test_jobsity/features/list_all_shows/presentation/store/list_all_shows_store.dart';
 import 'package:test_jobsity/features/show_details/presentation/store/show_details_state.dart';
 
 import '../../../../core/utils/remove_html_tags_from_string.dart';
@@ -25,135 +27,19 @@ class _ShowDetailsPageState extends ConsumerState<ShowDetailsPage> {
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      final store = ref.read(showDetailsStoreProvider.notifier);
-      store.getShowDetails(id: widget.showId);
+      final storeDetails = ref.read(showDetailsStoreProvider.notifier);
+      final storeFavorites = ref.read(favoritesShowsStoreProvider.notifier);
+      storeDetails.getShowDetails(id: widget.showId);
+      storeDetails.isFavorite = storeFavorites.verifyItsFavorite(widget.showId);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(showDetailsStoreProvider);
-    final store = ref.read(showDetailsStoreProvider.notifier);
+    final storeDetails = ref.read(showDetailsStoreProvider.notifier);
 
-    if (state is LoadingShowDetailsState) {
-      return const Expanded(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    } else if (state is SuccessShowDetailsState) {
-      return Material(
-        child: SafeArea(
-          child: Container(
-            color: backgroundColor,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back_ios,
-                          color: primaryColor,
-                        ),
-                      ),
-                      const Spacer(),
-                      Text(
-                        state.show?.name ?? '',
-                        style: title,
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 200,
-                          width: 140,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: CachedNetworkImage(
-                              fit: BoxFit.cover,
-                              imageUrl: state.show!.image ??
-                                  'https://via.placeholder.com/140x200',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 20),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Schedule',
-                              style: title.copyWith(color: primaryColor),
-                            ),
-                            Text(
-                              'Days: ${state.show!.schedule?.days?.join('\n')}\nTime: ${state.show!.schedule?.time}',
-                              style: title.copyWith(fontSize: 12),
-                            ),
-                            Text(
-                              'No network',
-                              style: title.copyWith(fontSize: 12),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Genres',
-                              style: title.copyWith(color: primaryColor),
-                            ),
-                            Text(
-                              state.show!.genres?.join('\n') ?? 'No genres',
-                              style: title.copyWith(fontSize: 12),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Summary',
-                        style: title.copyWith(color: primaryColor),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: Text(
-                      removeAllHtmlTags(state.show!.summary ?? 'No summary'),
-                      style: title.copyWith(fontSize: 12),
-                      textAlign: TextAlign.justify,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SeasonsListBuilder(seasons: state.seasons!),
-                  const SizedBox(height: 100),
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
-    } else if (state is ErrorShowDetailsState) {
+    if (state is ErrorShowDetailsState) {
       return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -172,7 +58,7 @@ class _ShowDetailsPageState extends ConsumerState<ShowDetailsPage> {
               style: ElevatedButton.styleFrom(
                 backgroundColor: primaryColor,
               ),
-              onPressed: () => store.getShowDetails(id: widget.showId),
+              onPressed: () => storeDetails.getShowDetails(id: widget.showId),
               child: Text('Try again', style: whiteTitle),
             ),
           ],
@@ -180,6 +66,182 @@ class _ShowDetailsPageState extends ConsumerState<ShowDetailsPage> {
       );
     }
 
+    return Material(
+      child: SafeArea(
+        child: Container(
+          color: backgroundColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context, () {
+                          ref
+                              .read(favoritesShowsStoreProvider.notifier)
+                              .getMyFavoritesShows();
+                          ref
+                              .read(listAllShowsStoreProvider.notifier)
+                              .getAllShows();
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                        color: primaryColor,
+                      ),
+                    ),
+                    const Spacer(),
+                    _buildTitle(state),
+                    const Spacer(),
+                    _buildFavoriteButton(state, storeDetails),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _buildMainInfoShow(state),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Summary',
+                      style: title.copyWith(color: primaryColor),
+                    ),
+                  ),
+                ),
+                _buildSummary(state),
+                const SizedBox(height: 20),
+                _buildSeasonsList(state),
+                const SizedBox(height: 100),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildFavoriteButton(ShowDetailsState state, ShowDetailsStore storeDetails) {
+    if (state is SuccessShowDetailsState) {
+      return IconButton(
+        onPressed: () {
+          setState(() {
+            storeDetails.isFavorite
+                ? storeDetails.removeFavoriteShow(state.show!.id)
+                : storeDetails.addFavoriteShow(state.show!);
+          });
+        },
+        icon: Icon(
+          storeDetails.isFavorite ? Icons.favorite : Icons.favorite_border,
+          color: storeDetails.isFavorite ? Colors.red : descriptionColor,
+        ),
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  _buildSeasonsList(state) {
+    if (state is SuccessShowDetailsState) {
+      return SeasonsListBuilder(seasons: state.seasons!);
+    }
+    return const SizedBox.shrink();
+  }
+
+  _buildTitle(ShowDetailsState state) {
+    if (state is LoadingShowDetailsState) {
+      return const SizedBox(
+        height: 10,
+        width: 10,
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is SuccessShowDetailsState) {
+      return Text(
+        state.show!.name ?? 'No name',
+        style: title.copyWith(color: primaryColor),
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  _buildSummary(ShowDetailsState state) {
+    if (state is LoadingShowDetailsState) {
+      return const SizedBox(
+        height: 20,
+        width: 20,
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is SuccessShowDetailsState) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Text(
+          removeAllHtmlTags(state.show!.summary ?? 'No summary'),
+          style: title.copyWith(fontSize: 12),
+          textAlign: TextAlign.justify,
+        ),
+      );
+    }
+    return const SizedBox.shrink();
+  }
+
+  _buildMainInfoShow(ShowDetailsState state) {
+    if (state is LoadingShowDetailsState) {
+      return const SizedBox(
+        height: 30,
+        width: 30,
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is SuccessShowDetailsState) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 200,
+              width: 140,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: CachedNetworkImage(
+                  fit: BoxFit.cover,
+                  imageUrl: state.show!.image ??
+                      'https://via.placeholder.com/140x200',
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Schedule',
+                  style: title.copyWith(color: primaryColor),
+                ),
+                Text(
+                  'Days: ${state.show!.schedule?.days?.join('\n')}\nTime: ${state.show!.schedule?.time}',
+                  style: title.copyWith(fontSize: 12),
+                ),
+                Text(
+                  'No network',
+                  style: title.copyWith(fontSize: 12),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Genres',
+                  style: title.copyWith(color: primaryColor),
+                ),
+                Text(
+                  state.show!.genres?.join('\n') ?? 'No genres',
+                  style: title.copyWith(fontSize: 12),
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    }
     return const SizedBox.shrink();
   }
 }
